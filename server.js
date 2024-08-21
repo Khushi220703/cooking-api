0ad693f77c9529ff1242d3f1c5d8b3801a41227a
Khushi220703 <122461997+Khushi220703@users.noreply.github.com>
2024-08-21 20:23:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,9 +9,7 @@ const mongoose = require("mongoose");
 dotenv.config();
 const app = express();
 
-app.use(cors({
-    origin: 'https://cooking-mhdk.onrender.com'
-}));
+app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
